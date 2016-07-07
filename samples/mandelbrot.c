299d5bce765657b67532ed7d68fa6d61a82170b1
Masayuki Takagi <kamonama@gmail.com>
2016-07-08 01:37:45 +1000

    Implement Mandelbrot sample code.

---
+++ samples/mandelbrot.c
@@ -0,0 +1,61 @@
+/*
+ *  To compile and run:
+ *    $ gcc -O3 -o mandelbrot mandelbrot.c
+ *    $ ./mandelbrot > mandelbrot.pgm
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+int main(int agc, char** argv)
+{
+    int* xs;
+    struct timeval start_time, end_time;
+
+    // Allocate memory.
+    xs = (int*)malloc(2048*2048*sizeof(int));
+    if (!xs) exit(-1);
+
+    // Get start time.
+    gettimeofday(&start_time, NULL);
+
+    // Compute Mandelbrot set.
+    for (int i = 0; i < 2048*2048; ++i) {
+        double x = 0.0, y = 0.0;
+        double a = (double)(i % 2048 - 512) / 1024.0;
+        double b = (double)(i / 2048 - 1024) / 1024.0;
+        xs[i] = 0;
+        for (int m = 1; m < 100; ++m) {
+            double x1 = x*x - y*y - a;
+            double y1 = 2*x*y - b;
+            if (x1*x1 + y1*y1 > 4.0) {
+                xs[i] = m;
+                break;
+            }
+            x = x1; y = y1;
+        }
+    }
+
+    // Get end time and show elapsed time.
+    gettimeofday(&end_time, NULL);
+    double sec = (double)(end_time.tv_sec - start_time.tv_sec);
+    double micro = (double)(end_time.tv_usec - start_time.tv_usec);
+    double passed = sec + micro / 1000.0 / 1000.0;
+    fprintf( stderr, "Elapsed: %f [sec]\n", passed );
+    
+    // Output Mandelbrot set in PGM format.
+    printf("P2\n");
+    printf("2048 2048\n");
+    printf("255\n");
+    for (int i = 0; i < 2048*2048; ++i) {
+        printf("%d\n", MIN(255, xs[i]*8));
+    }
+
+    // Free memory.
+    free(xs);
+
+    return 0;
+}
--- samples/mandelbrot.lisp
+++ samples/mandelbrot.lisp
@@ -5,28 +5,49 @@
 
 (in-package :cl-user)
 (defpackage foo.samples.mandelbrot
-  (:use :cl)
+  (:use :cl
+        :foo)
   (:export :main))
 (in-package :foo.samples.mandelbrot)
 
 
-(defkernel-symbol-macro nmax 100)
+;(defkernel-symbol-macro nmax 100)
 
-(defkernel mandelbrot (xs x y n)
-  (if (< n nmax)
-      (let ((x1 (- (* x x) (* y y) i))
-            (y1 (- (* 2.0d0 x y) j)))
-        (if (> (+ (* x1 x1) (* y1 y1)) 4.0d0)
-            (set (aref xs i j) n)
-            (mandelbrot xs x y (+ n 1))))
-      (set (aref i j) 0)))
+(defkernel mandelbrot (xs x y m)
+  (let ((a (/ (coerce (- (mod i 2048) 512)) 1024.0d0))
+        (b (/ (coerce (- (/ i 2048) 1024)) 1024.0d0)))
+    (if (< m 100)
+        (let ((x1 (- (* x x) (* y y) a))
+              (y1 (- (* 2.0d0 x y) b)))
+          (if (> (+ (* x1 x1) (* y1 y1)) 4.0d0)
+              (set (aref xs i) m)
+              (mandelbrot xs x1 y1 (+ m 1))))
+        (set (aref xs i) 0))))
 
-(defun draw-mandelbrot (xs)
-  (declare (ignore xs))
-  (error "Not implemented."))
+(defun draw-mandelbrot (pathname xs)
+  (with-open-file (out pathname :direction :output
+                                :if-does-not-exist :create
+                                :if-exists :supersede)
+    (write-line "P2" out)
+    (write-line "2048 2048" out)
+    (write-line "255" out)
+    (dotimes (i (* 2048 2048))
+      (princ (min 255 (* 8 (array-aref xs i))) out)
+      (terpri out))))
 
-(defun main ()
+(defun main (&optional dev-id)
   (with-cuda (dev-id)
-    (with-arrays ((xs int (32 32)))
-      (mandelbrot xs 0.0d0 0.0d0 1)
-      (draw-mandelbrot xs))))
+    (with-arrays ((xs int (* 2048 2048)))
+      (time
+       (mandelbrot xs 0.0d0 0.0d0 1))
+      (draw-mandelbrot #P"~/Desktop/mandelbrot.pgm" xs))))
+
+#+nil
+(eval-when (:compile-toplevel :load-toplevel)
+  (require :sb-sprof))
+#+nil
+(defun profile (&optional dev-id)
+  (sb-sprof:with-profiling (:max-samples 1000
+                            :report      :graph
+                            :loop        nil)
+    (main dev-id)))
